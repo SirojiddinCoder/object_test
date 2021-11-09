86989a075e2ff72d3f05cf792a6b60e910f29851
SirojiddinCoder <SirojiddinCoder9998@gmail.com>
2021-11-09 08:56:00 -0800

    Equal obj true falsee

--- main.js
+++ main.js
@@ -1,4 +1,12 @@
 
+// 1. Object kalitlari va qiymatlarini tengligini tekshirish
+// const data = { a: 1, b: 1 };
+// const data2 = { a: 1, b: 1 };
+// const data3 = { a: 1, b: 2 };
+
+// areEqual( data, data2 )  => true
+// areEqual( data, data3 )  => false
+
 const EqualObjects = (a, b) => {
     let s = (o) => Object.entries(o).sort().map(i => {
        if(i[1] instanceof Object) i[1] = s(i[1]);
