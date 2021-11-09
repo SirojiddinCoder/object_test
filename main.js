a5cd1b6bdc4467cfd334197ccbfb9319890cdde8
SirojiddinCoder <SirojiddinCoder9998@gmail.com>
2021-11-09 08:54:44 -0800

    Equal obj true false

--- main.js
+++ main.js
@@ -1,51 +1,10 @@
-var global = (function() {
-    return this;
- })();
 
+const EqualObjects = (a, b) => {
+    let s = (o) => Object.entries(o).sort().map(i => {
+       if(i[1] instanceof Object) i[1] = s(i[1]);
+       return i
+    })
+    return JSON.stringify(s(a)) === JSON.stringify(s(b))
+  }
 
-
- function Pair(key, value) {
-     this.key = key;
-     this.value = value;
-
-     this.toString = function() {
-        return "(" + key + ", " + value + ")";
-     };
- }
-
-
- Object.prototype.as = function(dataName) {
-     var value, key, data;
-     global[dataName] = data = [];
-
-     for (key in this) {
-        if (this.hasOwnProperty(key)) {
-           value = this[key];
-
-           (function() {
-              var k = key,
-                  v = value;
-
-             data.push(new Pair(k, v));
-           })();
-        }
-     }
-
-     return data;
- };
-
- var d = {
-    'one': 1,
-    'two': 2
- };
-
-
- for (var i = 0, max = d.as("data").length; i < max; i += 1) {
-    key = data[i].key;
-    value = data[i].value;
-
-    console.log("key: " + key + ", value: " + value);
- }
-
-
- delete data;
+  console.log(EqualObjects({ a: 1, b: 1 }, { a: 1, b: 2 }));
